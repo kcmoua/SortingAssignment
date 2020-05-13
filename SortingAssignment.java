c1e38a65fa5193d2b33503a26a8372c8058d8966
Keng Moua <keng@Kengs-MacBook-Pro.local>
2020-05-12 21:19:52 -0400

    updated file

--- SortingAssignment.java
+++ SortingAssignment.java
@@ -7,11 +7,11 @@ public class SortingAssignment {
         for(int i=0;i<100;i++) {
             randNums.add(i, (Math.floor(Math.random()*100)));
         }
-        Collections.sort(randNums);
+        Collections.sort(randNums);     // sort in ascending order
         System.out.println(randNums);
         System.out.println();
         // Collections.reverse(randNums);
-        Collections.sort(randNums, Collections.reverseOrder());
+        Collections.sort(randNums, Collections.reverseOrder());     // sort in descending order
         System.out.println(randNums);
     }
 }
